e6c8aa23b9f6ca25417061d45d120a4add36b43d
Adrian Borup <adrian@borup.com>
2025-10-19 11:31:55 +0200

    Make bees rotate by flowers

--- Assets/Scripts/BeeFlyingSystem.cs
+++ Assets/Scripts/BeeFlyingSystem.cs
@@ -17,12 +17,24 @@ partial struct BeeFlyingSystem : ISystem
     [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
-        var move = new BeeMoveJob
+        EntityCommandBuffer.ParallelWriter ecb =
+            SystemAPI.GetSingleton<BeginSimulationEntityCommandBufferSystem.Singleton>()
+                .CreateCommandBuffer(state.WorldUnmanaged).AsParallelWriter();
+
+        var deltaTime = (float)SystemAPI.Time.DeltaTime;
+
+        var flyToFlowerJob = new BeeToFlowerJob
         {
-            deltaTime = (float)SystemAPI.Time.DeltaTime,
+            ecb = ecb,
+            deltaTime = deltaTime,
         }.Schedule(state.Dependency);
 
-        move.Complete();
+        var atFlowerJob = new BeeAtFlowerJob
+        {
+            deltaTime = deltaTime,
+        }.Schedule(flyToFlowerJob);
+
+        atFlowerJob.Complete();
     }
 
     [BurstCompile]
@@ -52,14 +64,31 @@ partial struct BeeFlyingSystem : ISystem
 }
 
 [BurstCompile]
-public partial struct BeeMoveJob : IJobEntity
+public partial struct BeeToFlowerJob : IJobEntity
+{
+    public EntityCommandBuffer.ParallelWriter ecb;
+    public float deltaTime;
+
+    void Execute([ChunkIndexInQuery] int chunkKey, Entity entity, ref LocalTransform trans, ref BeeData bee, in TravellingToFlower travellingToFlower)
+    {
+        var moved = BeeFlyingSystem.TravelBee(ref trans, ref bee, deltaTime);
+
+        if (!moved)
+        {
+            ecb.RemoveComponent<TravellingToFlower>(chunkKey, entity);
+            ecb.AddComponent(chunkKey, entity, new AtFlower());
+        }
+    }
+}
+
+[BurstCompile]
+public partial struct BeeAtFlowerJob : IJobEntity
 {
     public float deltaTime;
 
-    void Execute(Entity entity, ref LocalTransform trans, ref BeeData bee, in TravellingToFlower travellingToFlower)
+    void Execute(Entity entity, ref LocalTransform trans, in AtFlower atFlower)
     {
-        var moved = BeeFlyingSystem.TravelBee(ref trans, ref bee, deltaTime);
-        
-        // if (!moved)
+        var rot = math.mul(quaternion.RotateZ(2f * deltaTime), quaternion.RotateY(2f * deltaTime));
+        trans.Rotation = math.mul(trans.Rotation, rot);
     }
 }
