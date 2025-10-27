99ba109e070cefdf0316a6d8411492f9d051f24e
Adrian Borup <adrian@borup.com>
2025-10-27 20:50:38 +0100

    Save scene

--- Assets/Scripts/FlowerManager.cs
+++ Assets/Scripts/FlowerManager.cs
@@ -5,12 +5,13 @@ using Unity.Entities;
 using Unity.Mathematics;
 using Random = Unity.Mathematics.Random;
 
-
+[BurstCompile]
 public struct FlowerManager : IComponentData
 {
     public NativeArray<Entity> flowerEntities;
     public NativeArray<FlowerData> flowerData;
 
+    [BurstCompile]
     public (Entity, FlowerData) GetRandomFlower(Random rng)
     {
         var index = rng.NextInt(0, flowerEntities.Length);
